a64d7084b59b399796252e9b7946c07325c7550b
Goivnd Kumar Yadav <robertgovind@gmail.com>
2023-12-31 13:49:22 +0545

    modified

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../coder.jpg";
+import image from "../images/coder.jpg";
 
 const imageAltText = "desktop with books and laptop with coding environment";
 
